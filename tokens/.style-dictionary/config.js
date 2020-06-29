4da18044f0f0a8e9be22cbcac5fb3998c7223b41
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2020-06-29 15:48:16 +0200

    Filter out inherited variables from output files
    In our design system we only want to use tokens defined within it. We
    pick WMF variables into our global files, but do not want to use them in
    alias and component-level tokens. Most certainly we do not want them to
    be used by consuming applications or showcase them in storybook for
    direct consumption.

--- tokens/.style-dictionary/config.js
+++ tokens/.style-dictionary/config.js
@@ -1,42 +1,47 @@
 'use strict';
 
-const StyleDictionary = require( 'style-dictionary' ).extend( {
-	include: [ 'node_modules/wikimedia-ui-base/tokens.json' ],
-	source: [ 'properties/**/*.json' ],
-	platforms: {
-		scss: {
-			transformGroup: 'scss',
-			buildPath: 'dist/',
-			files: [ {
-				destination: '_variables.scss',
-				format: 'scss/variables',
-			} ],
+const { removeWikimediaUiBaseVars } = require( './lib' ),
+	StyleDictionary = require( 'style-dictionary' ).extend( {
+		include: [ 'node_modules/wikimedia-ui-base/tokens.json' ],
+		source: [ 'properties/**/*.json' ],
+		platforms: {
+			scss: {
+				transformGroup: 'scss',
+				buildPath: 'dist/',
+				files: [ {
+					destination: '_variables.scss',
+					format: 'scss/variables',
+					filter: removeWikimediaUiBaseVars,
+				} ],
+			},
+			css: {
+				transformGroup: 'css',
+				buildPath: 'dist/',
+				files: [ {
+					destination: 'variables.css',
+					format: 'css/variables',
+					filter: removeWikimediaUiBaseVars,
+				} ],
+			},
+			less: {
+				transformGroup: 'less',
+				buildPath: 'dist/',
+				files: [ {
+					destination: '_variables.less',
+					format: 'less/variables',
+					filter: removeWikimediaUiBaseVars,
+				} ],
+			},
+			json: {
+				transformGroup: 'web',
+				buildPath: 'dist/',
+				files: [ {
+					destination: 'index.json',
+					format: 'json',
+					filter: removeWikimediaUiBaseVars,
+				} ],
+			},
 		},
-		css: {
-			transformGroup: 'css',
-			buildPath: 'dist/',
-			files: [ {
-				destination: 'variables.css',
-				format: 'css/variables',
-			} ],
-		},
-		less: {
-			transformGroup: 'less',
-			buildPath: 'dist/',
-			files: [ {
-				destination: '_variables.less',
-				format: 'less/variables',
-			} ],
-		},
-		json: {
-			transformGroup: 'web',
-			buildPath: 'dist/',
-			files: [ {
-				destination: 'index.json',
-				format: 'json',
-			} ],
-		},
-	},
-} );
+	} );
 
 StyleDictionary.buildAllPlatforms();
