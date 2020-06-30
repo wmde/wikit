436092ed970bb37150e96f8acbb830345eec0ac6
Pablo Grass <pablo.grass@wikimedia.de>
2020-06-30 10:10:56 +0200

    docs: polish a few story details

--- docs/src/color.stories.js
+++ docs/src/color.stories.js
@@ -2,39 +2,40 @@ import { color } from '@wmde/wikit-tokens';
 
 /**
  * Gets specific set of information about every color in a subset of the color tokens
- * @param {json} colorType an object containing a certain subset of colors, e.g. base, accent, utility etc.
- * @returns {Array} An array of objects containing information (title, subtitle, hex) about a certain color
+ *
+ * @param {Object.<string, {name: string, value: string, original: any}>} colorType An object
+ *   containing a certain subset of colors, e.g. base, accent, utility etc.
+ * @return {{title: string, subtitle: string, hex: string}[]} An array of objects containing
+ *   information about a certain color
  */
-const getColorInfo = ( colorType ) => {
-    return Object.keys( colorType ).map( color => {
-        return {
-            title: colorType[ color ].name,
-            subtitle: colorType[ color ].original.value.replace( /{|}|.value/gi, '' ),
-            hex: colorType[ color ].value
-        };
-    } );
+function getColorInfo( colorType ) {
+    return Object.keys( colorType ).map( color => ( {
+        title: colorType[ color ].name,
+        subtitle: colorType[ color ].original.value.replace( /{|}|.value/gi, '' ),
+        hex: colorType[ color ].value
+    } ) );
 }
 
-export const colorBase = () => {
+function flattenColorInfo( values ) {
+	let tokens = [];
+	for ( const topic in values ) {
+		tokens = tokens.concat( getColorInfo( values[ topic ] ) );
+	}
+	return tokens;
+}
+
+export function colorBase() {
     return getColorInfo( color.base );
 }
 
-export const colorAccent = () => {
+export function colorAccent() {
     return getColorInfo( color.accent );
 }
 
-export const colorUtility = () => {
-    let allUtilityColors = [];
-    for ( const utilityColor in color.utility ) {
-        allUtilityColors = allUtilityColors.concat( getColorInfo( color.utility[ utilityColor ] ) );
-    }
-    return allUtilityColors;
+export function colorUtility() {
+	return flattenColorInfo( color.utility );
 }
 
-export const colorModifier = () => {
-    let allModifierColors = [];
-    for ( const modifierColor in color.modifier ) {
-        allModifierColors = allModifierColors.concat( getColorInfo( color.modifier[ modifierColor ] ) );
-    }
-    return allModifierColors;
+export function colorModifier() {
+	return flattenColorInfo( color.modifier );
 }
