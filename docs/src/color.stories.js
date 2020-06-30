988a3794340a7938a06da8f42b1ef9bc02083e74
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2020-06-30 14:19:36 +0200

    Use traverse for nested token structures

--- docs/src/color.stories.js
+++ docs/src/color.stories.js
@@ -1,41 +1,23 @@
-import { color } from '@wmde/wikit-tokens';
+import traverse from 'traverse';
 
 /**
  * Gets specific set of information about every color in a subset of the color tokens
  *
- * @param {Object.<string, {name: string, value: string, original: any}>} colorType An object
+ * @param {Object} colorType A subtree of the tokens object containing only colors
  *   containing a certain subset of colors, e.g. base, accent, utility etc.
  * @return {{title: string, subtitle: string, hex: string}[]} An array of objects containing
  *   information about a certain color
  */
-function getColorInfo( colorType ) {
-	return Object.keys( colorType ).map( ( color ) => ( {
-		title: colorType[ color ].name,
-		subtitle: colorType[ color ].original.value.replace( /{|}|.value/gi, '' ),
-		hex: colorType[ color ].value,
-	} ) );
-}
+export function getColorInfo( colorType ) {
+	return traverse( colorType ).reduce( ( colors, node ) => {
+		if ( !node.original ) {
+			return colors;
+		}
 
-function flattenColorInfo( values ) {
-	let tokens = [];
-	for ( const topic in values ) {
-		tokens = tokens.concat( getColorInfo( values[ topic ] ) );
-	}
-	return tokens;
-}
-
-export function colorBase() {
-	return getColorInfo( color.base );
-}
-
-export function colorAccent() {
-	return getColorInfo( color.accent );
-}
-
-export function colorUtility() {
-	return flattenColorInfo( color.utility );
-}
-
-export function colorModifier() {
-	return flattenColorInfo( color.modifier );
+		return colors.concat( {
+			title: node.name,
+			subtitle: node.original.value.replace( /{|}|.value/gi, '' ),
+			hex: node.value,
+		} );
+	}, [] );
 }
