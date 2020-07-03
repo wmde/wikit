81042059680e04a51f5de9d9423c7f2d13644d9b
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2020-07-03 11:53:16 +0200

    docs: Extract repetitive token table code

---
+++ docs/src/lib/TokenTable.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { flattenTokenTree } from './flattenTokenTree';
+import { components } from '@storybook/components/dist/typography/DocumentFormatting';
+
+export function TokenTable( { tokens, valueCell } ) {
+	const renderValue = ( value ) => valueCell ? valueCell( value ) : <pre>{value}</pre>;
+
+	return (
+		<components.table style={{ width: '100%' }}>
+			<thead>
+			<tr>
+				<th>Name</th>
+				<th>Source</th>
+				<th>Value</th>
+			</tr>
+			</thead>
+			<tbody>
+			{
+				flattenTokenTree( tokens ).map( ( { name, referencedTokens, value } ) => (
+					<tr key={name} id={name}>
+						<td><a href={'#' + name}>{name}</a></td>
+						<td>{referencedTokens || '-'}</td>
+						<td>{renderValue( value )}</td>
+					</tr>
+				) )
+			}
+			</tbody>
+		</components.table>
+	);
+}
