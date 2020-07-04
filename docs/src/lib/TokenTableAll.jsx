37e43fa3736c65899aa9bae4ddbadc157076844b
Pablo Grass <pablo.grass@wikimedia.de>
2020-07-04 07:29:46 +0200

    docs: use Default presenter for property-type tokens

--- docs/src/lib/TokenTableAll.jsx
+++ docs/src/lib/TokenTableAll.jsx
@@ -10,38 +10,56 @@ import FontSize from './presenters/FontSize';
 import FontFamily from './presenters/FontFamily';
 import Transition from './presenters/Transition';
 import Cursor from './presenters/Cursor';
+import Default from './presenters/Default';
+
+/**
+ * Is a token used to denote another (CSS) property,
+ * e.g. "transition-property-box-shadow" = "box-shadow"
+ *
+ * @param name
+ * @return {boolean}
+ */
+function isPropertyToken( name ) {
+	return !!name.match(/\bproperty\b/);
+}
 
 function render( token ) {
-	const value = token.value;
-	if ( token.name.match(/\bcolor\b/) ) {
-		return <ColorItem key={ token.name }
-			title={ token.name }
+	const name = token.name;
+
+	if ( isPropertyToken( name ) ) {
+		return <Default token={ token } />;
+	}
+
+	if ( name.match(/\bcolor\b/) ) {
+		return <ColorItem key={ name }
+			title={ name }
 			subtitle={ token.referencedTokens }
 			colors={[ token.value ]}
 		/>;
 	}
-	else if ( token.name.match( /\bfont-family\b/ ) ) {
+	else if ( name.match( /\bfont-family\b/ ) ) {
 		return <FontFamily token={ token } />;
 	}
-	else if ( token.name.match( /\bfont-size\b/ ) ) {
+	else if ( name.match( /\bfont-size\b/ ) ) {
 		return <FontSize token={ token } />;
 	}
-	else if ( token.name.match( /\bfont-weight\b/ ) ) {
+	else if ( name.match( /\bfont-weight\b/ ) ) {
 		return <FontWeight token={ token } />;
 	}
-	else if ( token.name.match( /\bline-height\b/ ) ) {
+	else if ( name.match( /\bline-height\b/ ) ) {
 		return <LineHeight token={ token } />
 	}
-	else if ( token.name.match( /\bbox-shadow\b/ ) ) {
+	else if ( name.match( /\bbox-shadow\b/ ) ) {
 		return <Shadows token={ token } />
 	}
-	else if ( token.name.match( /\btransition\b/ ) ) {
+	else if ( name.match( /\btransition\b/ ) ) {
 		return <Transition token={ token } />
 	}
-	else if ( token.name.match( /\bcursor\b/ ) ) {
+	else if ( name.match( /\bcursor\b/ ) ) {
 		return <Cursor token={ token } />
 	}
-	return <pre>{value}</pre>;
+
+	return <Default token={ token } />;
 }
 
 export function TokenTableAll( { tokens } ) {
