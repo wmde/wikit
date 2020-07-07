c240249dba22a39bf3a094d0b508c09ec832a505
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2020-07-07 13:25:32 +0200

    docs: Improve line height storybook page
    Bug: T256855

--- docs/src/lib/presenters/LineHeight.jsx
+++ docs/src/lib/presenters/LineHeight.jsx
@@ -4,7 +4,8 @@ import React from 'react';
 export default function ( { token } ) {
 	return (
 		<div className='lineHeight' style={{ lineHeight: token.value }}>
-			The quick brown fox jumps over the lazy dog
+			The quick brown jumps<br />
+			over the lazy dog<br />
 		</div>
 	);
 }
