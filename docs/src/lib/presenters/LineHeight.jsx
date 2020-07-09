aebaf98c168f1f9b11f0f4b10709f5986be85dff
Pablo Grass <pablo.grass@wikimedia.de>
2020-07-09 15:56:05 +0200

    docs: demo line heights at various font sizes
    The idea was mentioned in a meeting - maybe it helps illustrate the line
    heights.
    
    Bug: T256855

--- docs/src/lib/presenters/LineHeight.jsx
+++ docs/src/lib/presenters/LineHeight.jsx
@@ -1,11 +1,24 @@
 import './LineHeight.css';
 import React from 'react';
 
+const demoFontSizes = [ '14px', '16px', '18px' ];
+
 export function LineHeight( { token } ) {
 	return (
-		<div className='lineHeight' style={{ lineHeight: token.value }}>
-			The quick brown jumps<br />
-			over the lazy dog<br />
-		</div>
+		<dl>
+			{
+				demoFontSizes.map( ( fontSize ) => {
+					return (
+						<div key={ fontSize } className='lineHeightContainer'>
+							<dt>@ { fontSize } font size</dt>
+							<dd className='lineHeight' style={{ lineHeight: token.value, fontSize }}>
+								The quick brown jumps<br />
+								over the lazy dog<br />
+							</dd>
+						</div>
+					);
+				} )
+			}
+		</dl>
 	);
 }
