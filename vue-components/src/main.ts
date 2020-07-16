3d5a832897bc3ec15572055da79e8b179eab9e7a
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2020-07-16 18:38:24 +0200

    Build as a library
    https://cli.vuejs.org/guide/build-targets.html#library

--- vue-components/src/main.ts
+++ vue-components/src/main.ts
@@ -1,8 +1,5 @@
-import Vue from 'vue'
-import App from './App.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+export {
+  HelloWorld
+}
