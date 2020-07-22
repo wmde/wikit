1f5a7096a88819e401fd7d5c47377f78d8160542
Pablo Grass <pablo.grass@wikimedia.de>
2020-07-22 18:32:15 +0200

    docs: compose vue-components' stories into main storybook
    This uses the URL of a chromatic feature branch storybook - which shows
    that the question _which_ version to show is the challenge.
    
    Using the chromatic "hack" to address a certain branch via
    https://branchname--appid.chromatic.com does not work because the fetch
    in the consuming storybook does not follow redirects.

--- docs/.storybook/main.js
+++ docs/.storybook/main.js
@@ -1,4 +1,10 @@
 module.exports = {
 	stories: [ '../src/**/*.stories.@(js|mdx)' ],
 	addons: [ '@storybook/addon-docs' ],
+	refs: {
+		vue: {
+			title: 'vue components',
+			url: 'https://5f186840fbe1720022e1ee98-dxykdieita.chromatic.com', // 'http://localhost:6005',
+		},
+	},
 };
