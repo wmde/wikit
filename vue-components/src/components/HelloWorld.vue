35a106d6d1c2a6d9a48076ccc56d91ef8704806a
Pablo Grass <pablo.grass@wikimedia.de>
2020-07-24 13:10:05 +0200

    vue-components: install SASS

--- vue-components/src/components/HelloWorld.vue
+++ vue-components/src/components/HelloWorld.vue
@@ -19,7 +19,7 @@ export default Vue.extend( {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
 h3 {
   margin: 40px 0 0;
 }
