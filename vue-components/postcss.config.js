4fc24a3b0afc7be992a7e51554e384f06cc0924c
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2020-07-24 11:17:19 +0200

    Make it possible to use logical properties
    The lib has to support RTL languages.
    CSS Logical Properties[0] offers the ability to control the layout
    through logical, rather than physical, direction and dimension mappings.
    They are supported in all major browsers but IE11[1].
    That's why this patch introduces two postcss plugins which transform
    1)css logical props and 2) the :dir selector[2]
    
    This should be sufficient for devs to write css that supports LTR and
    RTL languages.
    
    https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Logical_Properties
    https://caniuse.com/#search=logical%20properties
    https://caniuse.com/#search=%3Adir

--- vue-components/postcss.config.js
+++ vue-components/postcss.config.js
@@ -2,5 +2,7 @@ module.exports = {
 	plugins: [
 		require( 'autoprefixer' ),
 		require( 'postcss-prefixwrap' )( '.wikit' ),
+		require( 'postcss-logical' )( { preserve: true } ),
+		require( 'postcss-dir-pseudo-class' ),
 	],
 };
