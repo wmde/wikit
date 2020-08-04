7d323f7c56e5c1380e4278787f325cd7b7ad20d9
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2020-08-04 11:54:56 +0200

    Add class 'wikit' to all component's root nodes

---
+++ vue-components/tests/unit/components.spec.ts
@@ -0,0 +1,13 @@
+import { mount } from '@vue/test-utils';
+import * as allComponents from '@/main';
+
+describe( 'All components', () => {
+	it( 'have the class wikit in their root node', () => {
+		Object.keys( allComponents ).forEach( ( component ) => {
+			// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+			// @ts-ignore
+			const wrapper = mount( allComponents[ component ] ); // { HelloWorld: VueConstructor<Vue>; }
+			expect( wrapper.classes() ).toContain( 'wikit' );
+		} );
+	} );
+} );
