c0daaafa4471e1bb054b026f3bb1d52847c8df11
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2020-08-04 18:26:02 +0200

    Add 'margin' and 'padding' to stylelint's blacklist
    We can't use margin and padding because css logical properties
    does not support them yet
    https://github.com/w3c/csswg-drafts/issues/1282

--- vue-components/stylelint.config.js
+++ vue-components/stylelint.config.js
@@ -8,5 +8,9 @@ module.exports = {
 	rules: {
 		'indentation': 'tab',
 		'csstools/use-logical': 'always',
+		/* eslint-disable */
+		// CSS Logical Properties do not support the shorthand 'margin' and 'padding' yet
+		// https://wmde.github.io/wikit/?path=/story/documentation-decisions-and-adrs-adrs-9-rtl-support--page#consequences
+		'property-blacklist': [ '/^margin$/', '/^padding$/' ],
 	},
 };
