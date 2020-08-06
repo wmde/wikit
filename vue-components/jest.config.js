76f53d3189044613fd8e53db3ed6c395231495f6
Pablo Grass <pablo.grass@wikimedia.de>
2020-08-06 13:08:03 +0200

    vue-components: tests: fail tests with console warn/error

--- vue-components/jest.config.js
+++ vue-components/jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
 	preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
 	setupFilesAfterEnv: [ '<rootDir>/tests/config/setup.ts' ],
+	testEnvironment: '<rootDir>/tests/config/JestCustomEnvironment.js',
 };
