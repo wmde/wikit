859caddf7db27c9f1585704f5d9768eb25b34738
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2020-08-07 16:25:21 +0200

    Compile vue storybook to es5

--- vue-components/.storybook/webpack.config.js
+++ vue-components/.storybook/webpack.config.js
@@ -6,6 +6,22 @@ module.exports = async ( { config } ) => {
 	config.resolve.alias[ '@' ] = path.resolve( __dirname, '../src' );
 
 	config.module.rules.push( {
+		test: /\.ts$/,
+		exclude: /node_modules/,
+		use: [
+			{
+				loader: 'ts-loader',
+				options: {
+					appendTsSuffixTo: [ /\.vue$/ ],
+					transpileOnly: true,
+					compilerOptions: {
+						target: 'es5',
+					},
+				},
+			},
+		],
+	} );
+	config.module.rules.push( {
 		test: /\.scss$/,
 		use: [
 			'vue-style-loader',
