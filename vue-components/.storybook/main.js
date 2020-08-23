29c929a5fe7cdcdda59b4fd3978622f7ef087818
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2020-08-23 16:36:09 +0200

    Install @storybook/addon-viewports

--- vue-components/.storybook/main.js
+++ vue-components/.storybook/main.js
@@ -4,5 +4,6 @@ module.exports = {
 		'@storybook/addon-a11y',
 		'@storybook/addon-docs',
 		'@storybook/addon-knobs',
+		'@storybook/addon-viewport',
 	],
 };
--- vue-components/.storybook/preview.ts
+++ vue-components/.storybook/preview.ts
@@ -1,7 +1,15 @@
 import { addDecorator } from '@storybook/vue';
 import { withA11y } from '@storybook/addon-a11y';
 import { withKnobs } from '@storybook/addon-knobs';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
 
 addDecorator( withA11y );
 
 addDecorator( withKnobs );
+
+export const parameters = {
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+    },
+  };
