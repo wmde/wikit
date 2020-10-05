125cce8175a0278b48f6994f133aef9a2256b6c2
Itamar Givon <itamar.givon@wikimedia.de>
2020-10-05 15:10:40 +0200

    Revert
    This reverts commit a2f43ba6b5043835ef692a6d1af143499bfa1f05.
    
    Revert "Avoid using derivatives of isVisible"
    
    This reverts commit 21bc8c67e323f25027dbbc1b5f2406f538262bd8.

--- vue-components/tests/e2e/specs/Button.test.js
+++ vue-components/tests/e2e/specs/Button.test.js
@@ -4,6 +4,6 @@ module.exports = {
 			.init()
 			.openComponentStory( 'button' )
 			.waitForElementPresent( '.wikit-Button' )
-			.assert.elementPresent( '.wikit-Button' );
+			.assert.visible( '.wikit-Button' );
 	},
 };
