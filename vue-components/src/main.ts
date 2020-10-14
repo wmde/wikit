2aa01fc495ab3068f467f17b01e79fa5617df2a0
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2020-10-14 17:33:25 +0200

    Create Lookup component
    The lookup component is a text input field that provides matching
    selectable suggestions as a user types into it. In the context of
    Wikidata, they can be used as Item and Property selectors, for example.
    
    This contains the initial implementation of the visual and interactive
    aspects of the Lookup component and its menu. The lookup behavior will
    be implemented in a separate step.
    
    Bug: T265008

--- vue-components/src/main.ts
+++ vue-components/src/main.ts
@@ -1,7 +1,9 @@
 import Button from './components/Button.vue';
 import TextInput from './components/TextInput.vue';
+import Lookup from './components/Lookup.vue';
 
 export {
 	Button,
 	TextInput,
+	Lookup,
 };
