0e609cc1f540f751d431b7b2e08d384a0f32fe4e
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2020-10-26 15:39:09 +0100

    Lookup browser tests
    created browser tests that check if
    
    + the menu appears on user input
    + no matches found text is displayed
    + selection indicator text is visible after selecting menu-item
    
    - keyboard nav tests not included

---
+++ vue-components/tests/e2e/specs/Lookup.test.js
@@ -0,0 +1,32 @@
+module.exports = {
+	'Lookup Menu shows on user input': ( client ) => {
+		client
+			.init()
+			.openComponentStory( 'lookup' )
+			.waitForElementPresent( '.wikit-Lookup' )
+			.setValue( 'input', 'potato' )
+			.waitForElementPresent( '.wikit-Lookup__menu' )
+			.assert.visible( '.wikit-Lookup__menu' );
+	},
+	'Lookup Menu displays no-results text on no matches found': ( client ) => {
+		client
+			.init()
+			.openComponentStory( 'lookup' )
+			.waitForElementPresent( '.wikit-Lookup' )
+			.setValue( 'input', 'whatever' )
+			.waitForElementPresent( '.wikit-Lookup__menu' )
+			.assert.containsText( '.wikit-LookupMenu__no-results', 'No match was found' );
+	},
+	'Lookup Menu selects menu item when LookupMenu-Item clicked': ( client ) => {
+		client
+			.init()
+			.openComponentStory( 'lookup' )
+			.waitForElementPresent( '.wikit-Lookup' )
+			.setValue( 'input', 'potato' )
+			.waitForElementPresent( '.wikit-Lookup__menu' )
+			.assert.visible( '.wikit-LookupMenu__item' )
+			.click( '.wikit-LookupMenu__item' )
+			.assert.containsText( '.selected-item-label', 'potato' )
+			.assert.containsText( '.selected-item-id', 'Q10998' );
+	},
+};
