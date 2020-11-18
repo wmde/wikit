9b6e20ebb4cd0c00913e637905a8e0357e33e534
Michael Große <mic.grosse@googlemail.com>
2020-11-18 15:57:29 +0100

    Remove unneeded storybook decorator to fix console warning
    This decorator is no longer needed and it caused an annoying warning in
    the console for every story.

--- vue-components/.storybook/preview.ts
+++ vue-components/.storybook/preview.ts
@@ -1,9 +1,4 @@
-import { addDecorator } from '@storybook/vue';
-import { withA11y } from '@storybook/addon-a11y';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
-
-
-addDecorator( withA11y );
 
 export const parameters = {
     viewport: {
