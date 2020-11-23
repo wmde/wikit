a52f427386e9954c2718bd515ac9de2a0236484a
Itamar Givon <itamar.givon@wikimedia.de>
2020-11-23 17:52:33 +0100

    Convert Lookup tests to BDD style

--- vue-components/tests/e2e/specs/Lookup.test.js
+++ vue-components/tests/e2e/specs/Lookup.test.js
@@ -1,50 +1,44 @@
-module.exports = {
-	'Lookup Menu shows on user input': ( client ) => {
+describe( 'Lookup Menu', function () {
+
+	beforeEach( function ( client ) {
 		client
+			.pause( 500 )
 			.init()
 			.openComponentStoryDirectory( 'lookup' )
 			.openComponentStory( 'lookup-lookup' )
-			.waitForElementPresent( '.wikit-Lookup' )
+			.waitForElementPresent( '.wikit-Lookup' );
+	} );
+
+	it( 'shows on user input', function ( client ) {
+		client
 			.setValue( 'input', 'potato' )
 			.waitForElementPresent( '.wikit-Lookup__menu' )
 			.assert.visible( '.wikit-Lookup__menu' );
-	},
-	'Lookup Menu displays no-results text on no matches found': ( client ) => {
+	} );
+
+	it( 'displays no-results text on no matches found', function ( client ) {
 		client
-			.pause( 500 )
-			.init()
-			.openComponentStoryDirectory( 'lookup' )
-			.openComponentStory( 'lookup-lookup' )
-			.waitForElementPresent( '.wikit-Lookup' )
 			.setValue( 'input', 'whatever' )
 			.waitForElementPresent( '.wikit-Lookup__menu' )
 			.assert.containsText( '.wikit-LookupMenu__no-results', 'No match was found' );
-	},
-	'Lookup Menu selects menu item when LookupMenu-Item clicked': ( client ) => {
+	} );
+
+	it( 'selects menu item when LookupMenu-Item clicked', function ( client ) {
 		client
-			.pause( 500 )
-			.init()
-			.openComponentStoryDirectory( 'lookup' )
-			.openComponentStory( 'lookup-lookup' )
-			.waitForElementPresent( '.wikit-Lookup' )
 			.setValue( 'input', 'potato' )
 			.waitForElementPresent( '.wikit-Lookup__menu' )
 			.assert.visible( '.wikit-LookupMenu__item' )
 			.click( '.wikit-LookupMenu__item' )
 			.assert.containsText( '.selected-item-label', 'potato' )
 			.assert.containsText( '.selected-item-id', 'Q10998' );
-	},
-	'Lookup emits first and last index on scroll change': ( client ) => {
+	} );
+
+	it( 'emits first and last index on scroll change', function ( client ) {
 		client
-			.pause( 500 )
-			.init()
-			.openComponentStoryDirectory( 'lookup' )
-			.openComponentStory( 'lookup-lookup' )
-			.waitForElementPresent( '.wikit-Lookup' )
 			.setValue( 'input', 'a' )
 			.waitForElementPresent( '.wikit-Lookup__menu' )
 			.execute( 'document.querySelectorAll(".wikit-LookupMenu__item")[7].scrollIntoView(false)' )
 			.assert.containsText( '.first-visible-element-index', '2' )
 			.assert.containsText( '.last-visible-element-index', '7' );
-	},
-};
+	} );
+} );
