2ee78044e8c9fd03c953333268b2cb83360ef77d
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2020-11-23 13:14:44 +0100

    [PoC] Remove init from test cases
    + testing if we really need to init the tests before each test
    removing them can potentially solve the problem of making too many
    calls to chromatic

--- vue-components/tests/e2e/specs/Lookup.test.js
+++ vue-components/tests/e2e/specs/Lookup.test.js
@@ -11,20 +11,14 @@ module.exports = {
 	},
 	'Lookup Menu displays no-results text on no matches found': ( client ) => {
 		client
-			.init()
-			.openComponentStoryDirectory( 'lookup' )
-			.openComponentStory( 'lookup-lookup' )
-			.waitForElementPresent( '.wikit-Lookup' )
+			.clearValue( 'input' )
 			.setValue( 'input', 'whatever' )
 			.waitForElementPresent( '.wikit-Lookup__menu' )
 			.assert.containsText( '.wikit-LookupMenu__no-results', 'No match was found' );
 	},
 	'Lookup Menu selects menu item when LookupMenu-Item clicked': ( client ) => {
 		client
-			.init()
-			.openComponentStoryDirectory( 'lookup' )
-			.openComponentStory( 'lookup-lookup' )
-			.waitForElementPresent( '.wikit-Lookup' )
+			.clearValue( 'input' )
 			.setValue( 'input', 'potato' )
 			.waitForElementPresent( '.wikit-Lookup__menu' )
 			.assert.visible( '.wikit-LookupMenu__item' )
@@ -34,10 +28,7 @@ module.exports = {
 	},
 	'Lookup emits first and last index on scroll change': ( client ) => {
 		client
-			.init()
-			.openComponentStoryDirectory( 'lookup' )
-			.openComponentStory( 'lookup-lookup' )
-			.waitForElementPresent( '.wikit-Lookup' )
+			.clearValue( 'input' )
 			.setValue( 'input', 'a' )
 			.waitForElementPresent( '.wikit-Lookup__menu' )
 			.execute( 'document.querySelectorAll(".wikit-LookupMenu__item")[7].scrollIntoView(false)' )
