a1f760d71c6afe7068e131d12094e390abb92e5b
Itamar Givon <itamar.givon@wikimedia.de>
2020-11-25 18:28:41 +0100

    Add semicolon

--- vue-components/tests/e2e/specs/Button.test.js
+++ vue-components/tests/e2e/specs/Button.test.js
@@ -2,7 +2,7 @@ describe( 'Button', function () {
 	beforeEach( function ( client ) {
 		client
 			.pause( 500 )
-			.init( client.launch_url + '/iframe.html?id=button--normal&viewMode=story' )
+			.init( client.launch_url + '/iframe.html?id=button--normal&viewMode=story' );
 	} );
 
 	it( 'is visible', function ( client ) {
