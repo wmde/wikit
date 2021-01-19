3a81222aaedb5dd9ce43b75bd258215ef1991a2e
Michael Große <mic.grosse@googlemail.com>
2021-01-19 17:51:24 +0100

    Silencing typescript errors

--- vue-components/src/components/ToggleButtonGroup.vue
+++ vue-components/src/components/ToggleButtonGroup.vue
@@ -1,14 +1,40 @@
 <script lang="ts">
-import Vue from 'vue';
+import Vue, { VNode } from 'vue';
+
+// FIXME: ideally this would have a return type like "component is typeof ToggleButton" -- how to do?
+function componentIsToggleButton( component: VNode ): boolean {
+	if ( !component.componentOptions ) {
+		return false;
+	}
+	return component.componentOptions.tag === 'ToggleButton';
+}
 
 export default Vue.extend( {
 	name: 'ToggleButtonGroup',
 	render( h ) {
-		const children = this.$scopedSlots.default();
-		children.forEach( ( button ) => {
-			button.componentOptions.propsData.listener=this.updateSingle;
+		if ( !this.$scopedSlots.default ) {
+			return h( 'span', 'Add buttons here' );
+		}
+		const children = this.$scopedSlots.default( {} ); // FIXME: how is the argument here used?
+		if ( !children ) {
+			return h( 'span', 'Add buttons here' );
+		}
+
+		children.forEach( ( button: VNode ) => {
+			if ( !componentIsToggleButton( button ) ) {
+				throw new Error( 'only ever give ToggleButtons to the ToggleButtonGroup!' );
+			}
+			// we now know we have a toggle button
+
+			// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+			// @ts-ignore
+			button.componentOptions.propsData.listener = this.updateSingle;
+			// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+			// @ts-ignore
 			const buttonValue = button.componentOptions.propsData.value;
-			button.componentOptions.propsData.isActive= buttonValue === this.value;
+			// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+			// @ts-ignore
+			button.componentOptions.propsData.isActive = buttonValue === this.value;
 		} );
 		return h( 'span', children );
 	},
