5639a2b9abe996d4e639d49b8c6ced0d5e15aecc
Michael Große <michael.grosse@wikimedia.de>
2021-01-26 18:55:17 +0100

    ToggleButton and ToggleButtonGroup (#358)
    * ToggleButton PoC
    
    * Second attempt: fix it in render
    
    * Silencing typescript errors
    
    * Use provide/inject
    
    * Fixed injection types
    
    * Add documentation to props and slots
    
    * Make ToggleButton ready for review
    
    * Fixes
    
    * Update vue-components/src/components/ToggleButton.vue
    
    Co-authored-by: Sai_San <sarai.sanchez@wikimedia.de>
    
    * Update vue-components/src/components/ToggleButton.vue
    
    Co-authored-by: Sai_San <sarai.sanchez@wikimedia.de>
    
    Co-authored-by: Amir Sarabadani <Ladsgroup@gmail.com>
    Co-authored-by: Sai_San <sarai.sanchez@wikimedia.de>

---
+++ vue-components/src/components/ToggleButtonGroup.vue
@@ -0,0 +1,84 @@
+<template>
+	<span class="wikit wikit-ToggleButtonGroup">
+		<slot />
+	</span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export interface ToggleButtonGroupInjection {
+	groupValue: ( () => string ) | null;
+	toggleListener: ( ( value: string ) => void ) | null;
+}
+
+export default Vue.extend( {
+	name: 'ToggleButtonGroup',
+	provide(): ToggleButtonGroupInjection {
+		return {
+			groupValue: (): string => this.value,
+			toggleListener: ( event: string ): void => {
+				this.$emit( 'input', event );
+			},
+		};
+	},
+	props: {
+		value: {
+			required: true,
+			type: String,
+		},
+	},
+} );
+</script>
+<style lang="scss">
+.wikit-ToggleButtonGroup :not(:last-child) {
+	border-inline-end: none;
+	border-start-end-radius: 0;
+	border-end-end-radius: 0;
+}
+
+.wikit-ToggleButtonGroup :not(:first-child) {
+	border-start-start-radius: 0;
+	border-end-start-radius: 0;
+}
+
+.wikit-ToggleButtonGroup :not(:first-child) {
+	border-start-start-radius: 0;
+	border-end-start-radius: 0;
+}
+
+.wikit-ToggleButtonGroup :disabled {
+	border-block: none;
+}
+
+.wikit-ToggleButtonGroup :focus {
+	outline: none;
+}
+
+.wikit-ToggleButtonGroup :not(:first-child):disabled {
+	border-inline-start-color: $color-base-100;
+}
+
+.wikit-ToggleButtonGroup {
+	[dir='ltr'] & :not(:first-child) {
+		border-top-left-radius: 0;
+		border-bottom-left-radius: 0;
+	}
+
+	[dir='rtl'] & :not(:first-child) {
+		border-top-right-radius: 0;
+		border-bottom-right-radius: 0;
+	}
+
+	[dir='ltr'] & :not(:last-child) {
+		border-top-right-radius: 0;
+		border-bottom-right-radius: 0;
+	}
+
+	[dir='rtl'] & :not(:last-child) {
+		border-top-left-radius: 0;
+		border-bottom-left-radius: 0;
+	}
+}
+
+</style>
