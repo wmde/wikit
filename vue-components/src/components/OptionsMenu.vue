2b516e2fd7126063db7a5f69ec70b9b825a87a46
Itamar Givon <itamar.givon.dev@gmail.com>
2021-02-01 14:37:03 +0100

    Apply minimum calculation for the max width (#362)
    * Apply minimum calculation for the max width
    * Fix clash between sass min and css min
    
    Bug: T269928

--- vue-components/src/components/OptionsMenu.vue
+++ vue-components/src/components/OptionsMenu.vue
@@ -187,7 +187,8 @@ $base: '.wikit-OptionsMenu';
 
 #{$base} {
 	min-width: $wikit-OptionsMenu-min-width;
-	max-width: $wikit-OptionsMenu-max-width;
+	/* Uses CSS min, Falls back on 95vw when max-width > viewport width */
+	max-width: #{min}($wikit-OptionsMenu-max-width, 95vw);
 	width: max-content;
 	background-color: $wikit-OptionsMenu-background-color;
 	border-radius: $wikit-OptionsMenu-border-radius;
