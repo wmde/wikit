e2be6266b889ba35daa98268ef0ab7376357132b
Bereket teshome <bereket.teshome@wikimedia.de>
2021-03-09 18:13:39 +0100

    - removed typo errors in styles

--- vue-components/src/components/Lookup.vue
+++ vue-components/src/components/Lookup.vue
@@ -117,7 +117,6 @@ export default defineComponent( {
 
 	&__label {
 		@include Label( block );
-		display: inine;
 	}
 }
 </style>
--- vue-components/src/components/TextInput.vue
+++ vue-components/src/components/TextInput.vue
@@ -95,7 +95,6 @@ export default defineComponent( {
 
 	&__label {
 		@include Label( block );
-		display: inine;
 	}
 }
 </style>
