24d93fbc4b99fefbae665a706e8ba85f301f7a1d
Sai_San <sarai.sanchez@wikimedia.de>
2021-03-16 10:54:29 +0100

    Add InputExtender tokens (#383)
    * Create InputExtender.json
    
    * Update InputExtender.json
    
    * Introduce LookupWithExtender (#387)
    
    * Introduce LookupWithExtender
    
    Bug: T275758
    
    * apply sans-serif font-family
    
    Lato wasn't working
    
    * Finish the InputWithExtender parts
    
    * Replace TextInput with Input
    
    To avoid validation message being weird.
    
    Co-authored-by: Sarai Sanchez <sarai.sanchez@wikimedia.de>
    
    Co-authored-by: Amir Sarabadani <Ladsgroup@gmail.com>

---
+++ vue-components/src/components/InputWithExtender.vue
@@ -0,0 +1,121 @@
+<template>
+	<div
+		:class="[ 'wikit', 'wikit-InputWithExtender' ]"
+	>
+		<span class="wikit-InputWithExtender__label-wrapper">
+			<label class="wikit-InputWithExtender__label" @click="$refs.select.focus()">{{ label }}</label>
+			<span><slot name="suffix" /></span>
+		</span>
+		<Input
+			:id="id"
+			:value="value"
+			@focus.native="showExtension = true"
+			@blur.native="showExtension = false"
+			@keydown.native.esc="showExtension = false"
+			:feedback-type="feedbackType"
+			@input="onInput"
+			:placeholder="placeholder"
+			:disabled="disabled"
+		/>
+		<div v-if="showExtension" class="wikit-InputWithExtender__extension">
+			<slot />
+		</div>
+		<ValidationMessage
+			v-if="error"
+			:type="error.type"
+			:message="error.message"
+		/>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import VueCompositionAPI, { computed, defineComponent } from '@vue/composition-api';
+import Input from '@/components/Input.vue';
+import ValidationMessage from '@/components/ValidationMessage.vue';
+import { ErrorProp, errorProp, getFeedbackTypeFromProps } from '@/compositions/validatable';
+import generateUid from '@/components/util/generateUid';
+
+Vue.use( VueCompositionAPI );
+
+export default defineComponent( {
+	name: 'InputWithExtender',
+	inheritAttrs: false,
+	components: {
+		Input,
+		ValidationMessage,
+	},
+	setup( props: { error: ErrorProp } ) {
+		return {
+			feedbackType: computed( getFeedbackTypeFromProps( props ) ),
+		};
+	},
+	data() {
+		return {
+			showExtension: false,
+			id: generateUid( 'wikit-InputWithExtender' ),
+		};
+	},
+	props: {
+		error: errorProp,
+		disabled: {
+			type: Boolean,
+			default: false,
+		},
+		placeholder: {
+			type: String,
+			default: '',
+		},
+		value: {
+			type: String,
+			default: null,
+		},
+		label: {
+			type: String,
+			default: null,
+		},
+	},
+	methods: {
+		onInput( value: string ): void {
+			this.showExtension = true;
+			this.$emit( 'input', value );
+		},
+	},
+} );
+</script>
+
+<style lang="scss">
+.wikit-InputWithExtender {
+	position: relative;
+
+	&__label-wrapper {
+		display: flex;
+		align-items: center;
+		gap: $dimension-spacing-small;
+	}
+
+	&__extension {
+		padding-inline: $wikit-InputExtender-padding-horizontal;
+		padding-block: $wikit-InputExtender-padding-vertical;
+		background: $wikit-InputExtender-background-color;
+		min-width: $wikit-InputExtender-min-width;
+		max-width: $wikit-InputExtender-max-width;
+		position: absolute;
+		overflow-y: auto;
+		z-index: 1;
+
+		border: $wikit-InputExtender-border-width $wikit-InputExtender-border-style $wikit-InputExtender-border-color;
+		box-shadow: $wikit-InputExtender-box-shadow;
+		border-radius: $wikit-InputExtender-border-radius;
+	}
+
+}
+</style>
+
+<style lang="scss">
+.wikit-InputWithExtender {
+	&__label {
+		@include Label( block );
+	}
+}
+</style>
