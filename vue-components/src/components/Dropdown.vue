6a7d81a6b9c7b848038fe6b61b114f2dfda83967
Sarai Sanchez <sarai.sanchez@wikimedia.de>
2021-04-07 19:03:18 +0200

    Apply right border and box-shadow on focus and hover
    This should make the warning and error dropdowns display the right styles on focus, but somehow it doesn't :-/

--- vue-components/src/components/Dropdown.vue
+++ vue-components/src/components/Dropdown.vue
@@ -208,14 +208,6 @@ $base: '.wikit-Dropdown';
 			}
 		}
 
-		&--warning {
-			border-color: $wikit-Dropdown-warning-border-color;
-		}
-
-		&--error {
-			border-color: $wikit-Dropdown-error-border-color;
-		}
-
 		&:hover {
 			border-color: $wikit-Dropdown-hover-border-color;
 			background-color: $wikit-Dropdown-hover-background-color;
@@ -226,6 +218,36 @@ $base: '.wikit-Dropdown';
 			border-color: $wikit-Dropdown-active-border-color;
 			background-color: $wikit-Dropdown-active-background-color;
 			box-shadow: $wikit-Dropdown-active-box-shadow;
+		}
+
+		&--warning {
+			border-color: $wikit-Dropdown-warning-border-color;
+			
+			&:hover {
+				border-color: $wikit-Dropdown-warning-border-color;
+				background-color: $wikit-Dropdown-hover-background-color;
+			}
+
+			&:focus,
+			&:active {
+				border-color: $wikit-Dropdown-warning-border-color;
+				box-shadow: $wikit-Dropdown-warning-active-box-shadow;
+			}
+		}
+
+		&--error {
+			border-color: $wikit-Dropdown-error-border-color;
+
+			&:hover {
+				border-color: $wikit-Dropdown-error-border-color;
+				background-color: $wikit-Dropdown-hover-background-color;
+			}
+
+			&:focus,
+			&:active {
+				border-color: $wikit-Dropdown-error-border-color;
+				box-shadow: $wikit-Dropdown-error-active-box-shadow;
+			}
 		}
 	}
 
