bf4e40ecd51e99a8899bb5e490a5f190506a90fe
Itamar Givon <itamar.givon.dev@gmail.com>
2021-08-26 12:13:10 +0200

    Add TextArea component (#459)
    * Upstream TextArea component from Mismatch Finder
    * Add TextArea story (#460)
    
    Co-authored-by: Silvan Heintze <59574251+Silvan-WMDE@users.noreply.github.com>
    Bug: T289138
    Bug: T289141

---
+++ vue-components/tests/unit/components/TextArea.spec.ts
@@ -0,0 +1,73 @@
+import { mount } from '@vue/test-utils';
+import TextArea from '@/components/TextArea.vue';
+import { ResizeLimit } from '@/components/ResizeLimit.ts';
+
+describe( 'TextArea.vue', () => {
+	it( 'accepts rows property', () => {
+		const wrapper = mount( TextArea, {
+			propsData: { rows: 42 },
+		} );
+
+		expect( wrapper.props().rows ).toBe( 42 );
+		expect( wrapper.find( 'textarea' ).attributes( 'rows' ) ).toBe( '42' );
+	} );
+
+	it( 'accepts resize property', () => {
+		const wrapper = mount( TextArea, {
+			propsData: { resize: ResizeLimit.Horizontal },
+		} );
+
+		expect( wrapper.props().resize ).toBe( ResizeLimit.Horizontal );
+		expect( wrapper.find( 'textarea' ).classes() ).toContain( 'wikit-TextArea__textarea--horizontal' );
+	} );
+
+	it( 'uses default resize value', () => {
+		const wrapper = mount( TextArea );
+
+		expect( wrapper.find( 'textarea' ).classes() ).toContain( 'wikit-TextArea__textarea--vertical' );
+	} );
+
+	it( 'throws on invalid resize values', () => {
+		expect( () => mount( TextArea, {
+			propsData: { resize: 'nonsense' },
+		} ) ).toThrow( 'Invalid prop: custom validator check failed for prop "resize"' );
+	} );
+
+	it( 'accepts a textarea value', () => {
+		const value = 'Some beautiful value!';
+		const wrapper = mount( TextArea, {
+			propsData: { value },
+		} );
+		const element = wrapper.find( 'textarea' ).element as HTMLFormElement;
+
+		expect( wrapper.props().value ).toBe( value );
+		expect( element.value ).toBe( value );
+	} );
+
+	it( 'accepts label property', () => {
+		const label = 'da Label';
+		const wrapper = mount( TextArea, {
+			propsData: { label },
+		} );
+
+		expect( wrapper.props().label ).toBe( label );
+		expect( wrapper.find( 'label' ).text() ).toBe( label );
+	} );
+
+	it( 'accepts placeholder property', () => {
+		const placeholder = 'This is a placeholder';
+		const wrapper = mount( TextArea, {
+			propsData: { placeholder },
+		} );
+
+		expect( wrapper.find( 'textarea' ).attributes( 'placeholder' ) ).toBe( placeholder );
+	} );
+
+	it( 'should emit a change event with textarea value', () => {
+		const userInput = 'hello';
+		const wrapper = mount( TextArea );
+
+		wrapper.find( 'textarea' ).setValue( userInput );
+		expect( wrapper.emitted( 'input' )![ 0 ] ).toEqual( [ userInput ] );
+	} );
+} );
