b2c0dc50cab6ebf828ca0af555107b472ee00666
Sarai Sanchez <sarai.sanchez@wikimedia.de>
2021-09-09 16:35:21 +0200

    Undo indentation changes

--- vue-components/stories/TextArea.stories.ts
+++ vue-components/stories/TextArea.stories.ts
@@ -7,18 +7,18 @@ export default {
 };
 
 export function basic( args: object ): Component {
-	return {
-		data(): object {
-			return {
-				// Binding to the currentValue prevents the value from resetting
-				// on the re-render when changing the read-only control from
-				// false to true
-				currentValue: '',
-			};
-		},
-		components: { TextArea },
-		props: Object.keys( args ),
-		template: `
+    return {
+        data(): object {
+            return {
+                // Binding to the currentValue prevents the value from resetting
+                // on the re-render when changing the read-only control from
+                // false to true
+                currentValue: ''
+            };
+        },
+        components: { TextArea },
+        props: Object.keys( args ),
+        template: `
 			<div style="max-width: 75%">
 				<TextArea 
                     :label="label"
@@ -33,112 +33,112 @@ export function basic( args: object ): Component {
                 />
 			</div>
 		`,
-		computed: {
-			validation(): string {
-				// To enable preview of validation states, we add this computed
-				// property and bind it to the component's error prop
-				const mapping = {
-					'none': null,
-					'warning': {
-						type: 'warning',
-						message: 'This is a warning message',
-					},
-					'error': {
-						type: 'error',
-						message: 'This is an error message',
-					},
-				};
+        computed: {
+            validation(): string {
+                // To enable preview of validation states, we add this computed
+                // property and bind it to the component's error prop
+                const mapping = {
+                    'none': null,
+                    'warning': {
+                        type: 'warning',
+                        message: 'This is a warning message'
+                    },
+                    'error': {
+                        type: 'error',
+                        message: 'This is an error message'
+                    }
+                }
 
-				return mapping[ this.error ];
-			},
-		},
-	};
+                return mapping[this.error];
+            },
+        }
+    };
 }
 
 basic.args = {
-	label: 'Label',
-	placeholder: 'Placeholder',
-	resize: 'vertical',
-	readOnly: false,
-	disabled: false,
-	loading: false,
-	error: 'none',
+    label: 'Label',
+    placeholder: 'Placeholder',
+    resize: 'vertical',
+    readOnly: false,
+    disabled: false,
+    loading: false,
+    error: 'none'
 };
 
 basic.argTypes = {
-	value: {
-		control: false,
-	},
-	error: {
-		control: {
-			type: 'select',
-			options: [ 'none', 'warning', 'error' ],
-			default: 'none',
-		},
-		table: {
-			type: {
-				summary: 'object',
-			},
-		},
-	},
-	label: {
-		control: {
-			type: 'text',
-		},
-	},
-	placeholder: {
-		control: {
-			type: 'text',
-		},
-	},
-	loading: {
-		control: {
-			type: 'boolean',
-		},
-	},
-	rows: {
-		control: {
-			type: 'range',
-			min: 2,
-			max: 20,
-		},
-	},
-	readOnly: {
-		control: {
-			type: 'boolean',
-		},
-	},
-	resize: {
-		table: {
-			defaultValue: {
-				summary: 'vertical',
-			},
-		},
-		control: {
-			type: 'select',
-			options: [ 'vertical', 'horizontal', 'both', 'none' ],
-			default: 'vertical',
-		},
-	},
-	input: {
-		description: 'Emitted on each character input to the textarea, contains the entire string value of the textarea itself.',
-		table: {
-			type: {
-				summary: 'string',
-			},
-		},
-	},
-	disabled: {
-		control: {
-			type: 'boolean',
-		},
-	},
+    value: {
+        control: false
+    },
+    error: {
+        control: {
+            type: 'select',
+            options: ['none', 'warning', 'error'],
+            default: 'none'
+        },
+        table: {
+            type: {
+                summary: 'object'
+            }
+        }
+    },
+    label: {
+        control: {
+            type: 'text',
+        },
+    },
+    placeholder: {
+        control: {
+            type: 'text',
+        },
+    },
+    loading: {
+        control: {
+            type: 'boolean',
+        }
+    },
+    rows: {
+        control: {
+            type: 'range',
+            min: 2,
+            max: 20
+        },
+    },
+    readOnly: {
+        control: {
+            type: 'boolean',
+        },
+    },
+    resize: {
+        table: {
+            defaultValue: {
+                summary: 'vertical'
+            }
+        },
+        control: {
+            type: 'select',
+            options: ['vertical', 'horizontal', 'both', 'none'],
+            default: 'vertical'
+        }
+    },
+    input: {
+        description: 'Emitted on each character input to the textarea, contains the entire string value of the textarea itself.',
+        table: {
+            type: {
+                summary: 'string'
+            }
+        }
+    },
+    disabled: {
+        control: {
+            type: 'boolean',
+        },
+    }
 };
 
 export function all(): Component {
-	return {
-		components: { TextArea },
-		template: `
+    return {
+        components: { TextArea },
+        template: `
             <div>
                 <div style="max-width: 95%;">
                     <TextArea label="Default" placeholder="Placeholder" />
@@ -160,5 +160,5 @@ export function all(): Component {
                 </div>
             </div>
         `,
-	};
+    };
 }
