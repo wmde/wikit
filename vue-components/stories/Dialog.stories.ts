9f945641de9cbd3d1ef403e76ad38fc67c64a5cf
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2021-11-30 16:49:56 +0100

    Add controls to complex Dialog

--- vue-components/stories/Dialog.stories.ts
+++ vue-components/stories/Dialog.stories.ts
@@ -7,84 +7,77 @@ export default {
 	title: '/Dialog',
 };
 
-export function normal(): Component {
+export function complex( args: { title: string, actions: string, dismissButton: boolean, visible: boolean, dialogText }): Component {
 	return {
 		components: { Button, Dialog },
+		props: Object.keys( args ),
 		template: `
 			<div>
 				<Button 
 					@click.native="$refs.simple.show()" 
 					variant="primary" type="progressive"
 				>
-					Open Simple Dialog
+					Open Basic Dialog
 				</Button>
 				<Dialog
-					title="This is a simple dialog"
+					:title="title"
 					ref="simple"
-					:actions="[
-						{
-							label: 'Close me',
-							namespace: 'close'
-						}
-					]"
+					:actions="actions"
 					style="transform: translate(50%, 50%)"
 					@action="(_, dialog) => dialog.hide()"
-					dismiss-button
-					visible
+					:dismiss-button="dismissButton"
+					:visible="visible"
 				>
 					<p style="margin-bottom:10px">Hello, I am a simple dialog</p>
-					<ul style="list-style-type: none;">
-						<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-						Fusce imperdiet metus eget ipsum euismod, non dictum velit 
-						consequat. Curabitur sit amet neque vitae eros placerat luct
-						us. Aenean sagittis, libero sed lacinia sollicitudin, </li>
-						<li>Vestibulum vitae tortor ac ipsum blandit vulputate.
-						Suspendisse eget nisi tortor. Vestibulum non odio pharetra</li>
-					</ul>
+					{{ dialogText }}
 				</Dialog>
 			</div>
 		`,
 	};
 }
 
-export function complex(): Component {
-	return {
-		components: { Button, Dialog },
-		// The primary button types are all in the same story to achieve high % of visual tests coverage.
-		// Do not use controls to change the type unless you actively decide that is better than having test coverage.
-		template: `
-			<div>
-				<Button 
-					@click.native="$refs.complex.show()" 
-					variant="primary" type="progressive"
-				>
-				Open Complex Dialog
-				</Button>
-				<Dialog
-					title="This is a complex dialog"
-					ref="complex"
-					:actions="[
-						{
-							label: 'Test Primary',
-							namespace: 'primary'
-						}
-					]"
-					style="transform: translate(50%, 50%)"
-					@action="(_, dialog) => dialog.hide()"
-					dismiss-button
-					visible
-				>
-					<h1 style="font-size:1.2rem;margin-bottom:1rem;">Hello, I am a complex dialog</h1>
-					<ul style="list-style-type: none;">
-						<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-						Fusce imperdiet metus eget ipsum euismod, non dictum velit 
-						consequat. Curabitur sit amet neque vitae eros placerat luct
-						us. Aenean sagittis, libero sed lacinia sollicitudin, </li>
-						<li>Vestibulum vitae tortor ac ipsum blandit vulputate.
-						Suspendisse eget nisi tortor. Vestibulum non odio pharetra</li>
-					</ul>
-				</Dialog>
-			</div>
-		`
-	};
-}
+complex.args = {
+	title: 'I am a basic dialog',
+	dismissButton: 'true',
+	actions: [
+		{
+			label: 'Close me',
+			namespace: 'close'
+		},
+		{
+			label: 'Secondary button',
+			namespace: 'secondary'
+		}
+	],
+	visible: true,
+	dialogText: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+		Fusce imperdiet metus eget ipsum euismod, non dictum velit 
+		consequat. Curabitur sit amet neque vitae eros placerat luct
+		us. Aenean sagittis, libero sed lacinia sollicitudin,
+
+		Vestibulum vitae tortor ac ipsum blandit vulputate.
+		Suspendisse eget nisi tortor. Vestibulum non odio pharetra`
+};
+
+complex.argTypes = {
+    title: {
+        control: {
+            type: 'text',
+        },
+    },
+	actions: {
+        control: {
+            type: 'object',
+        },
+    },
+	dismissButton: {
+        control: {
+            type: 'boolean',
+        },
+    },
+	visible: {
+        control: {
+            type: 'boolean',
+        },
+    },
+};
