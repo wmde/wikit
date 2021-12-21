45b05cd5e15021cefeeea7a373d72dc377297cfc
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2021-12-21 09:40:05 +0100

    Correct lint errors

--- vue-components/tests/e2e/specs/Dialog.test.js
+++ vue-components/tests/e2e/specs/Dialog.test.js
@@ -18,24 +18,24 @@ describe( 'Dialog', function () {
 	it( 'traps page focus, so that only visually focused elements are tab-able', function ( client ) {
 		client
 			.keys( client.Keys.TAB )
-			.elementActive( function( result ) {
+			.elementActive( function ( result ) {
 				client.elementIdText( result.value.ELEMENT, ( element ) => {
-					client.assert.equal( element.value, 'Primary action' )
-				  } );
+					client.assert.equal( element.value, 'Primary action' );
+				} );
 			} )
 			.keys( client.Keys.TAB )
-			.elementActive( function( result ) {
+			.elementActive( function ( result ) {
 				client.elementIdText( result.value.ELEMENT, ( element ) => {
 					// this one is to make sure that the tab is working
-					client.assert.equal( element.value, 'Secondary action' )
-				  } );
+					client.assert.equal( element.value, 'Secondary action' );
+				} );
 			} )
 			// makes sure it goes back to the first element after three tabs
 			.keys( client.Keys.TAB )
 			.keys( client.Keys.TAB )
-			.elementActive( function( result ) {
+			.elementActive( function ( result ) {
 				client.elementIdText( result.value.ELEMENT, ( element ) => {
-					client.assert.equal( element.value, 'Primary action' )
+					client.assert.equal( element.value, 'Primary action' );
 				} );
 			} );
 	} );
