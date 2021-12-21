eb974eaf23ac67573e5c0a3363586843b8beb9a4
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2021-12-21 12:26:39 +0100

    Add test of body scrollbars

--- vue-components/tests/e2e/specs/Dialog.test.js
+++ vue-components/tests/e2e/specs/Dialog.test.js
@@ -4,7 +4,12 @@ describe( 'Dialog', function () {
 		client
 			.pause( 500 )
 			.init( client.launch_url + '/iframe.html?id=dialog--complex&viewMode=story' )
-			.click( '.wikit-Button' )
+			// focus button to open Dialog
+			.keys( client.Keys.TAB )
+			.execute( 'document.body.style.height = "150vh";' )
+			.execute( 'window.scrollTo( 0, document.body.scrollHeight );' )
+			// hit enter to open Dialog after scroll. a click brings the button back into focus
+			.keys( client.Keys.ENTER )
 			.pause( 500 )
 			.waitForElementPresent( '.wikit-Dialog' );
 	} );
@@ -40,4 +45,14 @@ describe( 'Dialog', function () {
 			} );
 	} );
 
+	it( 'prevents underlying page from scrolling when opened and on initial render', function ( client ) {
+
+		client
+			// make sure we have scrolled down the top of the page
+			.assert.not.visible( '#root > div > .wikit-Button' )
+			.execute( 'return document.documentElement.scrollHeight > document.documentElement.clientHeight;',
+				function ( result ) {
+					this.assert.not.ok( result.value === false, 'body does not show scrollbars' );
+				} );
+	} );
 } );
