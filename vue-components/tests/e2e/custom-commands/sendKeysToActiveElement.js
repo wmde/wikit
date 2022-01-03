45dff26d94805b0d71958930c079ca8c2064ea4c
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2022-01-03 12:28:22 +0100

    Make elementActive work for firefox

--- vue-components/tests/e2e/custom-commands/sendKeysToActiveElement.js
+++ vue-components/tests/e2e/custom-commands/sendKeysToActiveElement.js
@@ -1,6 +1,8 @@
 exports.command = function ( keys ) {
 	this.elementActive( ( active ) => {
-		this.elementIdAttribute( active.value.ELEMENT, 'class', ( { value: classNames } ) => {
+		const activeElementId = Object.values( active.value )[ 0 ];
+
+		this.elementIdAttribute( activeElementId, 'class', ( { value: classNames } ) => {
 			this.sendKeys(
 				`.${classNames.split( ' ' ).join( '.' )}`,
 				keys,
--- vue-components/tests/e2e/specs/Dialog.test.js
+++ vue-components/tests/e2e/specs/Dialog.test.js
@@ -5,11 +5,11 @@ describe( 'Dialog', function () {
 			.pause( 500 )
 			.init( client.launch_url + '/iframe.html?id=dialog--complex&viewMode=story' )
 			// focus button to open Dialog
-			.keys( client.Keys.TAB )
+			.sendKeysToActiveElement( client.Keys.TAB )
 			.execute( 'document.body.style.height = "150vh";' )
 			.execute( 'window.scrollTo( 0, document.body.scrollHeight );' )
 			// hit enter to open Dialog after scroll. a click brings the button back into focus
-			.keys( client.Keys.ENTER )
+			.sendKeysToActiveElement( client.Keys.ENTER )
 			.pause( 500 )
 			.waitForElementPresent( '.wikit-Dialog' );
 	} );
@@ -22,24 +22,27 @@ describe( 'Dialog', function () {
 
 	it( 'traps page focus, so that only visually focused elements are tab-able', function ( client ) {
 		client
-			.keys( client.Keys.TAB )
+			.sendKeysToActiveElement( client.Keys.TAB )
 			.elementActive( function ( result ) {
-				client.elementIdText( result.value.ELEMENT, ( element ) => {
+				const activeElementId = Object.values( result.value )[ 0 ];
+				client.elementIdText( activeElementId, ( element ) => {
 					client.assert.equal( element.value, 'Primary action' );
 				} );
 			} )
-			.keys( client.Keys.TAB )
+			.sendKeysToActiveElement( client.Keys.TAB )
 			.elementActive( function ( result ) {
-				client.elementIdText( result.value.ELEMENT, ( element ) => {
+				const activeElementId = Object.values( result.value )[ 0 ];
+				client.elementIdText( activeElementId, ( element ) => {
 					// this one is to make sure that the tab is working
 					client.assert.equal( element.value, 'Secondary action' );
 				} );
 			} )
 			// makes sure it goes back to the first element after three tabs
-			.keys( client.Keys.TAB )
-			.keys( client.Keys.TAB )
+			.sendKeysToActiveElement( client.Keys.TAB )
+			.sendKeysToActiveElement( client.Keys.TAB )
 			.elementActive( function ( result ) {
-				client.elementIdText( result.value.ELEMENT, ( element ) => {
+				const activeElementId = Object.values( result.value )[ 0 ];
+				client.elementIdText( activeElementId, ( element ) => {
 					client.assert.equal( element.value, 'Primary action' );
 				} );
 			} );
