eb5354a577ae8db6b42aa26985145681f4c62b0c
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2022-01-05 18:44:34 +0100

    Improve dialog browser tests (#520)
    * Use custom command to send key press to active element
    
    This isn't a very robust solution, but it gets rid of the conditionals
    to make up for the different behavior of `.sendKeys( 'body', ... )`
    across browsers.
    
    * Run skipped test in safari (#521)

--- vue-components/tests/e2e/specs/Dialog.test.js
+++ vue-components/tests/e2e/specs/Dialog.test.js
@@ -26,46 +26,31 @@ describe( 'Dialog', function () {
 	} );
 
 	it( 'traps page focus, so that only visually focused elements are tab-able', function ( client ) {
-
 		const currentBrowser = client.options.desiredCapabilities.browserName;
 
-		// exclude internet explorer for now because there is a known bug in the .keys
-		// and .sendKeys functions
-		// ie: https://github.com/nightwatchjs/nightwatch/issues/2536
-		// skipping safari because there are some display issues in safari that need
-		// to be solved first. See: https://phabricator.wikimedia.org/T298551
-		if ( currentBrowser === 'internet explorer' || currentBrowser === 'safari' ) {
-			return;
+		if ( currentBrowser === 'internet explorer' ) {
+			return; // https://phabricator.wikimedia.org/T298238
 		}
 		client
 			.click( '.wikit-Button' )
 			.pause( 500 )
 			.waitForElementPresent( '.wikit-Dialog' )
-			.sendKeys( 'body', client.Keys.TAB )
+			.sendKeysToActiveElement( client.Keys.TAB )
 			.elementActive( function ( result ) {
 				client.elementIdText( getWebElementIdFromActiveElementResult( result ), ( element ) => {
 					const elementText1 = element.value.replace( /\s+/g, ' ' ).trim();
 					client.assert.equal( elementText1, 'Primary action' );
 				} );
 			} )
-			// sending multiple keys inside array will fail in Firefox.
-			// See: https://github.com/nightwatchjs/nightwatch/issues/2468#issuecomment-738089219
-			.sendKeys( 'body',
-				currentBrowser !== 'firefox' ?
-					client.Keys.TAB + client.Keys.TAB : client.Keys.TAB )
+			.sendKeysToActiveElement( client.Keys.TAB )
 			.elementActive( function ( result ) {
 				client.elementIdText( getWebElementIdFromActiveElementResult( result ), ( element ) => {
 					const elementText2 = element.value.replace( /\s+/g, ' ' ).trim();
 					client.assert.equal( elementText2, 'Secondary action' );
 				} );
 			} )
-			// different browsers are counting the combinations differently.
-			// firefox starts in the previous selected element,
-			// while chrome starts the tab count on each elementActive call
-			// chrome: makes sure it goes back to the first element after four tabs
-			.sendKeys( 'body', currentBrowser !== 'firefox' ?
-				client.Keys.TAB + client.Keys.TAB + client.Keys.TAB + client.Keys.TAB
-				: client.Keys.TAB + client.Keys.TAB )
+			.sendKeysToActiveElement( client.Keys.TAB )
+			.sendKeysToActiveElement( client.Keys.TAB )
 			.elementActive( function ( result ) {
 				client.elementIdText( getWebElementIdFromActiveElementResult( result ), ( element ) => {
 					const elementText3 = element.value.replace( /\s+/g, ' ' ).trim();
@@ -75,44 +60,48 @@ describe( 'Dialog', function () {
 	} );
 
 	it( 'prevents underlying page from scrolling when opened and on initial render', function ( client ) {
-		if ( client.options.desiredCapabilities.browserName !== 'internet explorer' ) {
-			client
-				.execute( 'document.body.style.height = "150vh";' )
-				.execute( 'window.scrollTo( 0, document.body.scrollHeight );' )
-				.execute( 'return document.documentElement.scrollHeight > document.documentElement.clientHeight;',
-					function ( result ) {
-						this.assert.ok( result.value === true, 'body shows scrollbars' );
-					} )
-				.click( '.wikit-Button' )
-				.pause( 250 )
-				.execute( 'return document.documentElement.scrollHeight > document.documentElement.clientHeight;',
-					function ( result ) {
-						this.assert.not.ok( result.value === false, 'body does not show scrollbars' );
-					} );
+		if ( client.options.desiredCapabilities.browserName === 'internet explorer' ) {
+			return; // https://phabricator.wikimedia.org/T298238
 		}
+
+		client
+			.execute( 'document.body.style.height = "150vh";' )
+			.execute( 'window.scrollTo( 0, document.body.scrollHeight );' )
+			.execute( 'return document.documentElement.scrollHeight > document.documentElement.clientHeight;',
+				function ( result ) {
+					this.assert.ok( result.value === true, 'body shows scrollbars' );
+				} )
+			.click( '.wikit-Button' )
+			.pause( 250 )
+			.execute( 'return document.documentElement.scrollHeight > document.documentElement.clientHeight;',
+				function ( result ) {
+					this.assert.not.ok( result.value === false, 'body does not show scrollbars' );
+				} );
 	} );
 
 	it( 'resets dialog scroll bars to top when closed and reopened', function ( client ) {
-		if ( client.options.desiredCapabilities.browserName !== 'internet explorer' ) {
-			client
-				.click( '.wikit-Button' )
-				.pause( 500 )
-				.waitForElementPresent( '.wikit-Dialog' )
-				// substract 100px from the dialog's original height so scrollbars are shown
-				.execute( `document.querySelector( ".wikit-Dialog__modal" ).style.height =
-					document.querySelector( ".wikit-Dialog__modal" ).offsetHeight - 100 + "px"` )
-				.execute( `document.querySelector(".wikit-Dialog__content" )
-					.scrollTo( 0, document.querySelector(".wikit-Dialog__content").scrollHeight)` )
-				.click( '.wikit-Dialog__close' )
-				// wait for animation
-				.pause( 500 )
-				.click( '.wikit-Button' )
-				// wait for animation
-				.pause( 500 )
-				.execute( 'return document.querySelector(".wikit-Dialog__content" ).scrollTop',
-					function ( result ) {
-						this.assert.ok( result.value === 0, 'dialog scrollbar is positioned at 0' );
-					} );
+		if ( client.options.desiredCapabilities.browserName === 'internet explorer' ) {
+			return; // https://phabricator.wikimedia.org/T298238
 		}
+
+		client
+			.click( '.wikit-Button' )
+			.pause( 500 )
+			.waitForElementPresent( '.wikit-Dialog' )
+			// substract 100px from the dialog's original height so scrollbars are shown
+			.execute( `document.querySelector( ".wikit-Dialog__modal" ).style.height =
+				document.querySelector( ".wikit-Dialog__modal" ).offsetHeight - 100 + "px"` )
+			.execute( `document.querySelector(".wikit-Dialog__content" )
+				.scrollTo( 0, document.querySelector(".wikit-Dialog__content").scrollHeight)` )
+			.click( '.wikit-Dialog__close' )
+			// wait for animation
+			.pause( 500 )
+			.click( '.wikit-Button' )
+			// wait for animation
+			.pause( 500 )
+			.execute( 'return document.querySelector(".wikit-Dialog__content" ).scrollTop',
+				function ( result ) {
+					this.assert.ok( result.value === 0, 'dialog scrollbar is positioned at 0' );
+				} );
 	} );
 } );
