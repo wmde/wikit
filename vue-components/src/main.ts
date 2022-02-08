d661136a7bd3f332b1ef9504800b49ee913d2aad
Michael Große <mic.grosse@googlemail.com>
2022-02-08 15:23:04 +0100

    Remove components that will not yet be migrated to vue 3 (#534)
    This should reduce the cognitive overhead and also make the tests run a
    bit faster and more stable.
    
    These components can be readded if and when we need to migrate them to
    vue 3.
    
    Bug: T301155

--- vue-components/src/main.ts
+++ vue-components/src/main.ts
@@ -1,39 +1,11 @@
 import Button from './components/Button.vue';
-import Checkbox from './components/Checkbox.vue';
-import DateInput from './components/DateInput.vue';
-import Dialog from './components/Dialog.vue';
-import ExtendedNumberInput from './components/ExtendedNumberInput.vue';
-import TextArea from './components/TextArea.vue';
-import Table from './components/Table.vue';
 import TextInput from './components/TextInput.vue';
-import ToggleButton from './components/ToggleButton.vue';
-import ToggleButtonGroup from './components/ToggleButtonGroup.vue';
-import Link from './components/Link.vue';
 import Lookup from './components/Lookup.vue';
-import Dropdown from './components/Dropdown.vue';
-import Message from './components/Message.vue';
-import Popover from './components/Popover.vue';
 import Icon from './components/Icon.vue';
-import BouncingDots from './components/BouncingDots.vue';
-import QuantityInput from './components/QuantityInput.vue';
 
 export {
 	Button,
-	Checkbox,
-	DateInput,
-	Dialog,
-	Dropdown,
-	ExtendedNumberInput,
-	Table,
-	TextArea,
 	TextInput,
-	ToggleButton,
-	ToggleButtonGroup,
-	Link,
 	Lookup,
-	Message,
-	Popover,
 	Icon,
-	BouncingDots,
-	QuantityInput,
 };
