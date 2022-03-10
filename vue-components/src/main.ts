33cea52182e0eab490fc9492356068e2b5cfef72
Michael Große <mic.grosse@googlemail.com>
2022-03-10 17:28:51 +0100

    Readd the original Message component
    These are the files copied directly from the current master branch of
    wikit. They will be adjusted in a follow-up commit to make reviewing the
    changes easier.

--- vue-components/src/main.ts
+++ vue-components/src/main.ts
@@ -1,10 +1,12 @@
 import Button from './components/Button.vue';
+import Message from './components/Message.vue';
 import TextInput from './components/TextInput.vue';
 import Lookup from './components/Lookup.vue';
 import Icon from './components/Icon.vue';
 
 export {
 	Button,
+	Message,
 	TextInput,
 	Lookup,
 	Icon,
