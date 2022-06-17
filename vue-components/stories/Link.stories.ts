fbd9fceab9b5ef495739a3a1d69be8ddf8894bd3
Michael Große <michael.grosse@wikimedia.de>
2022-06-17 11:27:55 +0200

    Migrate Link component to the next branch (#597)
    This is needed for the help-link to the spelling variant input
    
    Bug: T298146

---
+++ vue-components/stories/Link.stories.ts
@@ -0,0 +1,58 @@
+import WikitLink from '@/components/Link';
+import { Component } from 'vue';
+
+export default {
+	component: WikitLink,
+	// the `/` prefix in the title is needed for "Message" to appear as a folded navigation item, and not a headline
+	title: '/Link',
+};
+
+export function basic( args: { content: string; underlined: boolean } ): Component {
+	return {
+		data: () => args,
+		components: { WikitLink },
+		template: `
+			<div>
+			<p><WikitLink
+                :underlined="underlined"
+                :href="'#' + Math.random()"
+            ><span v-html="content" /></WikitLink></p>
+			</div>
+		`,
+	};
+}
+
+basic.args = {
+	underlined: false,
+	content: 'Controllable link',
+};
+
+basic.argTypes = {
+	underlined: {
+		control: {
+			type: 'boolean',
+		},
+	},
+	content: {
+		control: {
+			type: 'text',
+		},
+	},
+};
+
+export function all(): Component {
+	return {
+		components: { WikitLink },
+		template: `
+			<div>
+            <p><WikitLink :href="'#' + Math.random()">Click here</WikitLink></p>
+            <p><WikitLink :href="'#' + Math.random()" underlined>Click here</WikitLink></p>
+            <p><WikitLink :href="'#' + Math.random()" icon="before">About Wikidata</WikitLink></p>
+            <p><WikitLink :href="'#' + Math.random()" icon="after">About Wikidata</WikitLink></p>
+            </div>
+		`,
+	};
+}
+all.parameters = {
+	controls: { hideNoControlsWarning: true },
+};
